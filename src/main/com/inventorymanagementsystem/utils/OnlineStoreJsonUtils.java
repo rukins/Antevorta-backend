570e2a7e485132dadb85df150f93727ca3a25a0e
Sergey <sergey.rukin1425@gmail.com>
2022-10-22 21:53:29 +0500

    update OnlineStoreJsonUtils

--- src/main/com/inventorymanagementsystem/utils/OnlineStoreJsonUtils.java
+++ src/main/com/inventorymanagementsystem/utils/OnlineStoreJsonUtils.java
@@ -4,22 +4,35 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.inventorymanagementsystem.model.OnlineStoreDetails;
 import com.inventorymanagementsystem.model.OnlineStoreType;
+
+import java.util.List;
 
 public class OnlineStoreJsonUtils {
     private final static Gson gson = new Gson();
 
-    public static String getString(Object onlineStoreJson) {
+    public static String getString(OnlineStoreDetails onlineStore) {
         JsonObject jsonObject = new JsonObject();
 
-        JsonElement jsonElement = JsonParser.parseString(gson.toJson(onlineStoreJson));
+        JsonElement jsonElement = JsonParser.parseString(gson.toJson(onlineStore));
+
+        jsonObject.add("onlineStore", jsonElement);
+
+        return jsonObject.toString();
+    }
+
+    public static String getString(List<OnlineStoreDetails> onlineStores) {
+        JsonObject jsonObject = new JsonObject();
+
+        JsonElement jsonElement = JsonParser.parseString(gson.toJson(onlineStores));
 
         jsonObject.add("onlineStores", jsonElement);
 
         return jsonObject.toString();
     }
 
-    public static String getString(Object onlineStoreJson, OnlineStoreType type) {
+    public static String getString(List<OnlineStoreDetails> onlineStoreJson, OnlineStoreType type) {
         JsonObject jsonObject = new JsonObject();
 
         JsonElement jsonElement = JsonParser.parseString(gson.toJson(onlineStoreJson));
