2ba3bc7908b463869a9af92bf70ac3b4ee085a38
Sergey <sergey.rukin1425@gmail.com>
2022-10-28 22:42:55 +0500

    add getId() and getTitle() methods and getClassByType() static method in AbstractOnlineStoreProduct

--- src/main/com/inventorymanagementsystem/onlinestore/AbstractOnlineStoreProduct.kt
+++ src/main/com/inventorymanagementsystem/onlinestore/AbstractOnlineStoreProduct.kt
@@ -1,3 +1,22 @@
 package com.inventorymanagementsystem.onlinestore
 
-abstract class AbstractOnlineStoreProduct
+import com.inventorymanagementsystem.model.OnlineStoreType
+import com.inventorymanagementsystem.onlinestore.amazon.AmazonOnlineStore
+import com.inventorymanagementsystem.onlinestore.shopify.ShopifyOnlineStore
+import com.inventorymanagementsystem.onlinestore.shopify.model.Product
+import kotlin.reflect.KClass
+
+abstract class AbstractOnlineStoreProduct {
+    abstract fun getId(): Long
+    abstract fun getTitle(): String
+
+    companion object {
+        @JvmStatic
+        fun getClassByType(type: OnlineStoreType): Class<out AbstractOnlineStoreProduct> {
+            return when (type) {
+                OnlineStoreType.SHOPIFY -> Product::class.java
+                OnlineStoreType.AMAZON -> TODO("not implemented")
+            }
+        }
+    }
+}
--- src/main/com/inventorymanagementsystem/onlinestore/shopify/model/Product.kt
+++ src/main/com/inventorymanagementsystem/onlinestore/shopify/model/Product.kt
@@ -3,10 +3,18 @@ package com.inventorymanagementsystem.onlinestore.shopify.model
 import com.inventorymanagementsystem.onlinestore.AbstractOnlineStoreProduct
 
 data class Product (
-    var id: Long? = null, var title: String? = null, var body_html: String? = null, var vendor: String? = null,
+    private var id: Long, private var title: String, var body_html: String? = null, var vendor: String? = null,
     var product_type: String? = null, var created_at: String? = null, var handle: String? = null,
     var updated_at: String? = null, var published_at: String? = null, var template_suffix: String? = null,
     var status: String? = null, var published_scope: String? = null, var tags: String? = null,
     var admin_graphql_api_id: String? = null, var variants: List<Variant>? = null, var options: List<Option>? = null,
     var images: List<Image>? = null, var image: Image? = null
-) : AbstractOnlineStoreProduct()
+) : AbstractOnlineStoreProduct() {
+    override fun getId(): Long {
+        return this.id
+    }
+
+    override fun getTitle(): String {
+        return this.title
+    }
+}
