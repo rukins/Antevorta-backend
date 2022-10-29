95f592d124a72b84d7dd5da54ab6513fdfb59579
Sergey <sergey.rukin1425@gmail.com>
2022-10-29 22:37:04 +0500

    update shopify.Product

--- src/main/com/inventorymanagementsystem/onlinestore/shopify/model/Product.kt
+++ src/main/com/inventorymanagementsystem/onlinestore/shopify/model/Product.kt
@@ -3,7 +3,7 @@ package com.inventorymanagementsystem.onlinestore.shopify.model
 import com.inventorymanagementsystem.onlinestore.AbstractOnlineStoreProduct
 
 data class Product (
-    private var id: Long, private var title: String, var body_html: String? = null, var vendor: String? = null,
+    private var id: Long? = null, private var title: String? = null, var body_html: String? = null, var vendor: String? = null,
     var product_type: String? = null, var created_at: String? = null, var handle: String? = null,
     var updated_at: String? = null, var published_at: String? = null, var template_suffix: String? = null,
     var status: String? = null, var published_scope: String? = null, var tags: String? = null,
@@ -11,10 +11,18 @@ data class Product (
     var images: List<Image>? = null, var image: Image? = null
 ) : AbstractOnlineStoreProduct() {
     override fun getId(): Long {
-        return this.id
+        return this.id!!
+    }
+
+    fun setId(id: Long?) {
+        this.id = id
     }
 
     override fun getTitle(): String {
-        return this.title
+        return this.title!!
+    }
+
+    fun setTitle(title: String?) {
+        this.title = title
     }
 }
