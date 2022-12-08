e6da2cf665f9103af8f85cc8da87c2f389c049c3
Sergey <sergey.rukin1425@gmail.com>
2022-12-08 13:40:33 +0500

    rename InventoryManagementSystemApplication to AntevortaApplication

--- src/main/com/antevorta/AntevortaApplication.java
+++ src/main/com/antevorta/AntevortaApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InventoryManagementSystemApplication {
+public class AntevortaApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(InventoryManagementSystemApplication.class, args);
+		SpringApplication.run(AntevortaApplication.class, args);
 	}
 }
