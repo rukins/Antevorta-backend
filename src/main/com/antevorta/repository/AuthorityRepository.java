813063beb059063c4f16932961460b21f7d9015a
Sergey <sergey.rukin1425@gmail.com>
2022-12-28 23:29:17 +0500

    add AuthorityRepository

---
+++ src/main/com/antevorta/repository/AuthorityRepository.java
@@ -0,0 +1,8 @@
+package com.antevorta.repository;
+
+import com.antevorta.model.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+
+}
